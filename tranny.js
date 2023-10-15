be4c8413a02ba346f23ebc2b27a81386fa9f8f91
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-10-15 23:22:44 +0200

    modul-1.1

---
+++ tranny.js
@@ -0,0 +1,34 @@
+"use strict"
+
+// Модуль 1
+
+const yearOfBirth = "Mango";
+console.log(yearOfBirth);
+
+console.log(typeof yearOfBirth)
+
+const name = "Victoria";
+console.log(typeof name)
+
+
+let age
+age = 2;
+console.log(age)
+
+
+// Конвертація значень до false
+console.log(Boolean(0))
+console.log(Boolean(""))
+console.log(Boolean(NaN))
+console.log(Boolean(undefined))
+console.log(Boolean(null))
+console.log(Boolean(false))
+
+// ще один варіант запису
+console.log(!!2)
+
+// ДЗ
+
+const productName = "Droid";
+const pricePerItem = 2000;
+
