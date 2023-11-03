179d2762c094f8934172043b35eef64c400181d3
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-03 02:16:18 +0100

    Update tranny.js

--- tranny.js
+++ tranny.js
@@ -1,6 +1,11 @@
 "use strict"
 
-              // Урок 1.1
+                       // Урок 1.1
+       
+
+
+
+
 
 // const yearOfBirth = "Mango";
 // console.log(yearOfBirth);
@@ -36,7 +41,10 @@
 
 
 
-          // Урок 1.2
+                               // Урок 1.2
+
+
+
 
 // const value = 20;
 // if (value === 20) {
@@ -160,7 +168,12 @@
 //     console.log("Не парне", i)
 // }
 
-        //  Урок 2.1
+
+
+
+                         //  Урок 2.1
+
+
 
 
 
@@ -454,7 +467,16 @@
 
 
 
-// Урок 2.2
+
+
+
+
+
+                              // Урок 2.2
+
+
+
+
 
 // function foo(a, b) {
 //         return a + b
@@ -687,7 +709,7 @@
 // removeCourse(name) - видаляє курс з колекції
 // updateCourse(oldName, newName) - замінює імя на нове
 
-const courses = ["HTML", "CSS", "JavaScript", "React"]
+// const courses = ["HTML", "CSS", "JavaScript", "React"]
 
 // function addCourse(name) {
 //         if (courses.includes(name)) {
@@ -730,3 +752,382 @@ const courses = ["HTML", "CSS", "JavaScript", "React"]
 
 
 
+
+
+                           // Урок 3.1
+
+
+ //    Обʼєкт
+
+// const user = ["Kate", 20, 300]
+
+// const user = {
+//         name: "Kate",
+//         skills: {
+//                 html: true,
+//                 css: true,
+//                 JS: false,
+//         }
+// }
+
+
+// Звернення до властивостей обʼєкта
+
+// console.log(user.name)
+// console.log("skills", user.skills)
+
+// const objectKey = "name";
+
+// console.log(user[objectKey]);
+// console.log(user["name"]);
+
+
+// Зміна значення властивості
+
+// const user = {
+//         name: "Alice",
+//         skills: {
+//                 html: true,
+//                 css: false,
+//                 react: false
+//         },
+// }
+
+// user.city = "Lviv";
+// user["age"] = 28;
+// console.log(user);
+
+// user.city = "Odessa";
+// user["age"] = 38;
+// console.log(user)
+
+
+
+// Object freeze
+
+// const user = {
+//         name: "Alice",
+//         skills: {
+//                 html: true,
+//                 css: false,
+//                 react: false
+//         },
+// }
+
+// Object.freeze(user);
+// user.city = "Lviv";
+// user.name = "Kate";
+// console.log(user);
+
+
+// Методи обʼєкта 
+
+// const user = {
+//         name: "Alice",
+//         skills: {
+//                 html: true,
+//                 css: false,
+//                 react: false
+//         },
+//         sayHello(city) {
+//                 // console.log(city)
+//                 console.log(`Hello, my name is ${this.name}`);
+//         }
+// }
+
+// user.sayHello("Lviv")
+
+// const obj = {
+//         name: "Kate",
+// }
+
+// obj.sayHello = user.sayHello
+// obj.sayHello()
+// console.log(obj);
+
+
+// Цикл for.... in
+
+// const user = {
+//         name: "Alice",
+//         skills: {
+//                 html: true,
+//                 css: false,
+//                 react: false
+//         },
+// }
+
+// for (const key in user) {
+//         console.log(key);
+//         // console.log(user[key]);
+// }
+
+
+
+// Метод Object.keys()
+
+// const user = {
+//         name: "Alice",
+//         skills: {
+//                 html: true,
+//                 css: false,
+//                 react: false
+//         },
+//         age: 10
+// }
+
+// const keys = Object.keys(user)
+// // console.log(keys);
+
+// for (const key of keys) {
+//         console.log(key, user[key]);
+// }
+
+// const pet = {
+//         legs: 4,
+// }
+
+// const dog = Object.create(pet)
+// dog.name = "Pess"
+// // console.log(dog);
+// // console.log(dog.name);
+// // console.log(dog.legs);
+
+// // for (const key in dog) {
+// //         console.log(dog[key]);
+// // }
+
+// const keys = Object.keys(dog);
+// // console.log(keys);
+
+// for (const key of keys) {
+//         console.log(key, dog[key]);
+// }
+
+
+
+// Метод Object.values()
+
+// const user = {
+//         name: "Alice",
+//         skills: {
+//                 html: true,
+//                 css: false,
+//                 react: false
+//         },
+// }
+
+// const values = Object.values(user)
+// // const values = Object.values(user.skills)
+// console.log(values);
+
+
+
+// Метод Object.entries()
+
+// const user = {
+//         name: "Alice",
+//         skills: {
+//                 html: true,
+//                 css: false,
+//                 react: false
+//         },
+// }
+
+// // const entries = Object.entries(user)
+// const entries = Object.entries(user.skills)
+// console.log(entries);
+
+
+
+// Завдання 1 
+// Створи функцію яка буде приймати 3 параметри та формувати
+// об'єкт покупки
+// 1 Назва продукту
+// 2 Кількість одиниць
+// 3 Ціна за 1 одиницю товару
+// Функція має повертати сформований об'єкт з ключами 
+// name, price, quantity, 
+// totalPrice.
+
+// function createBasket(product, quantity, price) {
+//         const totalPrice = quantity * price;
+
+// //         const basket = {
+// //                 name: product,
+// //                 price,
+// // quantity,
+// // totalPrice
+// //         }
+//         return {
+//                 name: product,
+//                 price,
+// quantity,
+// totalPrice
+//         }
+// }
+
+// console.log(createBasket("pizza", 3, 120));
+// console.log(createBasket("apple", 5, 200));
+
+
+// Task - 2
+// Створи функцію для ведення статистики комп'ютерного клубу. 
+// Функція приймає 1 параметр це об'єкт користувачів 
+// де ключ це ім'я 
+// користувача, а значення це час оренди у хвилинах.
+// Функція повертає рядок з інформацією про те скільки 
+// було користувачів 
+// та який середній час оренди комп'ютера.
+
+// const players = {
+//            Den: 60,
+//     Kate: 130,
+//     William: 45,
+//     Matthew: 120,
+//     Ethan: 40,
+//     David: 55,
+// }
+
+// function getTime(obj) {
+//         const values = Object.values(players)
+//         let total = 0;
+
+//         for (const value of values) {
+//                 total += value
+//         }
+//         console.log(total);
+//         return `Count of players ${values.length}, average time ${total / values.length}`
+// }
+
+// console.log(getTime(players));
+
+
+// Task - 3
+// 1 Створити функцію яка буде приймати 2 параметри
+//    1 параметр масив
+//    2 параметр назва книги
+// Функція повертає Імена юзерів (формат стрінги) 
+// в яких є дана книга ("Harry Potter") \\\ "Anna, Oleksii"
+
+// 2 Порахувати вік всіх юзерів у яких є ключ age.
+
+// const friends = [
+//     { name: "Anna", books: ["Bible", "Harry Potter"], age: 21 },
+//     { name: "Bob", books: ["War and peace", "Romeo and Juliet"], age: 26 },
+//     { name: "Alice", books: ["War and peace", "Romeo and Juliet"] },
+//     { name: "Oleksii", books: ["Bible", "War and peace", "Harry Potter", "Romeo and Juliet"], age: 26 },
+// ]
+
+// function getUsers(arr, bookName) {
+//         const userNames = [];
+//         for (const user of arr) {
+//                 // console.log(user.books);
+//                 if (user.books.includes(bookName)) {
+//                         userNames.push(user.name)
+//                 }
+                
+//         }
+//         return userNames.join(",")
+// }
+
+// console.log(getUsers(friends, "Harry Potter"));
+
+// function getTotalAge(arr) {
+//         let totalAge = 0;
+
+//         for (const user of arr) {
+//                 if ("age" in user) {
+//                         totalAge += user.age
+//                 }
+//         }
+//         return totalAge
+// }
+
+// console.log(getTotalAge(friends));
+
+
+// Task - 4
+// 1 Створити метод об'єкту який буде приймати 1 
+// параметр назву факультету 
+// та повертати списoк імен учнів факультету
+// 2 Створити метод об'єкту який буде приймати 1 параметр
+// назву факультету 
+// та повертати кількість очків факультету
+
+// const hogvarts = {
+//         griffindor: [{
+//                 name: "Harry",
+//                 points: 17
+//         },
+//         {
+//                 name: "Hermiona",
+//                 points: 19
+//         },
+//         {
+//                 name: "Ron",
+//                 points: 14
+//                 }],
+        
+//         slizerin: [{
+//                 name: "Draco",
+//                 points: 17
+//         },
+//         {
+//                 name: "Goyl",
+//                 points: 14
+//         },
+//         {
+//                 name: "Crabbe",
+//                 points: 5
+//                 }],
+        
+//         getUserList(faculty) {
+//                 if (!(faculty in this)) {
+//                         return "Not found "
+//                 }
+
+//                 const students = [];
+                
+//                 for (const student of this[faculty]) {
+//                         students.push(student.name)
+//                 }
+//                 return students.join(",")
+//         }
+        
+//         getTotalPoints(faculty) {
+//           if (!(faculty in this)) {
+//                         return "Not found"
+//           }
+        
+//         let totalPoints = 0;
+
+//                 for (const student of this[faculty]) {
+//                         if ("points" in student) {
+//                                 totalPoints += student.points;
+//        }
+//                 }
+//                 return totalPoints
+//         }
+// }
+
+
+// console.log(hogvarts.getUserList('griffindor'));
+// console.log(hogvarts.getUserList('slizerin'));
+// console.log(hogvarts.getUserList('slitherin'));
+
+
+// console.log(hogvarts.getTotalPoints('griffindor'));
+// console.log(hogvarts.getTotalPoints('slizerin'));
+// console.log(hogvarts.getTotalPoints('slitherin'));
+
+
+
+
+
+                //      Урок 3.2
+
+
+
+
+                
