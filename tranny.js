8517e521695bf65311e9f409ab2f98e058edf69d
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-19 01:11:43 +0100

    tranny

--- tranny.js
+++ tranny.js
@@ -1237,7 +1237,7 @@
 //    з деструктуризацією
 
 
-// 1)
+// 1
 // const names = [];
 
 // for (const {name} of users) {
@@ -1247,7 +1247,7 @@
 // console.log(names)
 
 
-// 2)
+// 2
 // const names = [];
 
 // for (const user of users) {
@@ -1258,16 +1258,249 @@
 // console.log(names)
 
 
-                    // Операція rest та spread 
+                    // Операція rest та spread
  
 // const numbers = [1, 2, 3];
 // const copy = [...numbers]; //spread - це ... (розгортає старий масив, бере данні і складає в новий масив)
 
 
 
-const numbers = [1, 2, 3];
-const [first, ...args] = numbers; //rest - збирає данні, які залишилися в новий масив
-console.log(args)
+// const numbers = [1, 2, 3];
+// const [first, ...args] = numbers; //rest - збирає данні, які залишилися в новий масив
+// console.log(args)
+
+
+// const obj = {
+//         a: 1,
+//         b: 2,
+//         c: false,
+// }
+
+// const { a, b, ...args } = obj;
+// console.log(args);
+
+
+                    // Операція rest та spread в функціях
+
+// const numbers = [1, 2, 3];
+
+// function foo(...props) { //rest
+        
+// }
+
+// foo(...numbers) //spread
+
+
+// const names = ["Alice", "Kate", "Emma"];
+
+// function foo(first, second, third) {
+//         console.log("first", first)
+//         console.log("second", second)
+//           console.log("third", third)
+// }
+
+// foo(...names) //foo("Alice", "Kate", "Emma") - spread оператоп
+//тотожно ось такому запису foo("Alice", "Kate", "Emma")
+
+
+
+// function foo(apple, second, ...args) { //rest
+//         console.log("first", apple)
+//         console.log("second", second)
+//         console.log(args);
+// }
+
+// foo("Alice", "Kate", "Emma", "Mila", "Victoria"); //spread
+
+
+// const user = {
+//         name: "test name",
+//         age: 22,
+//         city: "Lviv",
+//         skills: {
+//                 html: true,
+//                 css: true,
+//                 js: false
+//         }
+// }
+
+// function foo({ name, age, ...props }) {
+//         console.log(name);
+//         console.log(age);
+//         console.log(props);
+// }
+
+// foo(user)
+
+
+// function foo({ name, skills: { html, ...props }, ...props2 }) {
+//         console.log(name);
+//         console.log(props);
+//         console.log(props2);
+//         console.log(html);
+// }
+
+// foo(user)
+
+
+
+
+
+// Task - 1 - Деструктуризація
+// Деструктуризуй об'єкт таким чином щоб отримати 
+// значення name, 
+// surname, username
+
+// const user = {
+//     id: 1,
+//     username: 'harry_potter',
+//     profile: {
+//       name: 'Harry',
+//       surname: 'Potter',
+//       age: 25
+//     }
+//   };
+
+// // Деструктуризація об'єкта для отримання окремих змінних
+// const { username, profile: { name, surname }} = user
+
+
+// // Виведення отриманих значень
+// console.log(`Ім'я користувача: ${name}`);
+// console.log(`Прізвище користувача: ${surname}`);
+// console.log(`Ім'я користувача (за нікнеймом): ${username}`);
+
+
+
+
+// Task - 2 Деструктуризація
+// Допиши функцію таким чином щоб кожна властивість об'єкта 
+// product була незалежним параметром
+
+// const product = {
+//     name: 'Smart TV',
+//     price: 25000,
+//     category: 'Electronics',
+//     details: {
+//       brand: 'Samsung',
+//       color: 'Black',
+//       weight: '15.5'
+//     }
+//   };
+
+// function displayProductInfo({name, price, category, details:{brand, color, weight}}) {
+//     console.log(`Назва товару: ${name}`);
+//     console.log(`Ціна: ${price} грн`);
+//     console.log(`Категорія: ${category}`);
+//     console.log('Деталі:');
+//     console.log(`- Бренд: ${brand}`);
+//     console.log(`- Колір: ${color}`);
+//     console.log(`- Вага: ${weight} кг`);
+// }
+
+// displayProductInfo(product);
+
+
+
+
+// Task - 3 - Операція spread
+// Напиши функцію createContact(partialContact) так, 
+// щоб вона повертала новий об'єкт контакту з доданими 
+// властивостями 
+// id та createdAt, а також list зі значенням "default" 
+// якщо в 
+// partialContact немає такої властивості.
+
+// function createContact(partialContact) {
+//         return {
+//                 id: Math.floor(Math.random() * 100),
+//                 createdAt: new Date(),
+//                 list: "default",
+//                 ...partialContact
+//        }
+
+// }
+
+// console.log(createContact({
+//         name: 'Mango',
+//         email: 'mango@mail.com',
+//         list: 'friends',
+//     }),
+// );
+
+// console.log(
+//     createContact({
+//         name: 'Poly',
+//         email: 'poly@hotmail.com',
+//     }),
+// );
+
+
+
+
+// Task - 4 - Операція rest
+// Напиши функцію transformUsername(user) так, 
+// щоб вона повертала 
+// новий об'єкт із властивістю fullName, 
+// замість firstName та lastName.
+
+// function transformUsername({ firstName, lastName, ...props }) {
+//         return {
+//                 fullName: `${firstName} ${lastName}`,
+//                 ...props
+
+//         }
+//         // console.log(props)
+
+// }
+
+// console.log(
+//     transformUsername({
+//         id: 1,
+//         firstName: 'Jacob',
+//         lastName: 'Mercer',
+//         email: 'j.mercer@mail.com',
+//         friendCount: 40,
+//     }),
+// );
+
+
+// console.log(
+//     transformUsername({
+//         id: 2,
+//         firstName: 'Adrian',
+//         lastName: 'Cross',
+//         email: 'a.cross@hotmail.com',
+//         friendCount: 20,
+//     }),
+// );
+
+
+
+
+
+// Task - 5 Об'єкт параметрів
+// Необхідно зрoбити рефакторинг функції 
+// calculateHousePerimeter, 
+// так щоб вона приймала об'єкт з параметрами будинку,
+// включаючи довжини сторін будинку. 
+// Функція повинна розрахувати та повернути периметр будинку.
+
+
+//  function calculateHousePerimeter({sideA, sideB, sideC, sideD}) {
+//    const perimeter = sideA + sideB + sideC + sideD
+//          return perimeter;
+// }
+
+// const house = {
+//     sideA: 10,
+//     sideB: 15,
+//     sideC: 10,
+//     sideD: 15
+// }
+
+// const perimeter = calculateHousePerimeter(house);
+// console.log(`Периметр будинку: ${perimeter}`);
 
 
 
@@ -1275,10 +1508,9 @@ console.log(args)
 
 
 
+                          // Урок 6.1
 
 
-
-                // Урок 6.1
 
 // const link = document.querySelector(".link");
 
