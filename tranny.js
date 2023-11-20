f1a044bb012ca0f1fd62cce81d775aa4a0923b30
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-20 02:22:06 +0100

    lesson-7.1

--- tranny.js
+++ tranny.js
@@ -2096,61 +2096,83 @@
 // (https://github.com/electerious/basicLightbox)
 
 
-const products = [
-    {
-      id: 1,
-      img: "https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG",
-      name: "Монітор",
-      price: 3000,
-      description: "23-дюймовий монітор з Full HD роздільною здатністю.",
-    },
-    {
-      id: 2,
-      img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzWqRMI3HQiDfICHAmbArmaP4uOOIjfz0sDITv0dfkpb0mbbgX",
-      name: "Ноутбук",
-      price: 20000,
-      description: "Легкий та потужний ноутбук з 15-дюймовим дисплеєм та SSD.",
-    },
-    {
-      id: 3,
-      img: "https://cdn.27.ua/799/66/39/6841913_1.jpeg",
-      name: "Смартфон",
-      price: 8000,
-      description: "Оснащений потрійною камерою та багатоядерним процесором.",
-    },
-    {
-      id: 4,
-      img: "https://cdn.27.ua/799/b6/16/4371990_1.jpeg",
-      name: "Планшет",
-      price: 12000,
-      description:
-        "10-дюймовий планшет з високою продуктивністю та Retina дисплеєм.",
-    },
-];
+// const products = [
+//     {
+//       id: 1,
+//       img: "https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG",
+//       name: "Монітор",
+//       price: 3000,
+//       description: "23-дюймовий монітор з Full HD роздільною здатністю.",
+//     },
+//     {
+//       id: 2,
+//       img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzWqRMI3HQiDfICHAmbArmaP4uOOIjfz0sDITv0dfkpb0mbbgX",
+//       name: "Ноутбук",
+//       price: 20000,
+//       description: "Легкий та потужний ноутбук з 15-дюймовим дисплеєм та SSD.",
+//     },
+//     {
+//       id: 3,
+//       img: "https://cdn.27.ua/799/66/39/6841913_1.jpeg",
+//       name: "Смартфон",
+//       price: 8000,
+//       description: "Оснащений потрійною камерою та багатоядерним процесором.",
+//     },
+//     {
+//       id: 4,
+//       img: "https://cdn.27.ua/799/b6/16/4371990_1.jpeg",
+//       name: "Планшет",
+//       price: 12000,
+//       description:
+//         "10-дюймовий планшет з високою продуктивністю та Retina дисплеєм.",
+//     },
+// ];
 
-const container = document.querySelector(".js-products");
+// const container = document.querySelector(".js-products");
 
-container.insertAdjacentHTML("beforeend", createMarkup(products))
-container.addEventListener("click", handleClick)
+// container.insertAdjacentHTML("beforeend", createMarkup(products))
+// container.addEventListener("click", handleClick)
 
-function createMarkup(arr) {
-        return arr.map(({ id, img, name, price, description }) => `
-                <li data-id="${id}" class="item js-product-item">
-                     <img src="${img}" alt="${name}" width="300px"/>
-                     <h2>${name}</h2>
-                     <p>Price: ${price}</p>
-                </li>
-        `).join("")
-};
-console.log(createMarkup(products));
+// function createMarkup(arr) {
+//         return arr.map(({ id, img, name, price, description }) => `
+//                 <li data-id="${id}" class="item js-product-item">
+//                      <img src="${img}" alt="${name}" width="300px"/>
+//                      <h2>${name}</h2>
+//                      <p>Price: ${price}</p>
+//                 </li>
+//         `).join("")
+// };
 
-function handleClick(event) {
-        if (event.target === event.currentTarget) {
-                return;
-        }
+// console.log(createMarkup(products));
 
-        const currentProduct = event.target.closest(".js-product-item") //цей метод повертає найближчий батьківський елемент з вказаним класом
-        // console.log("currentTarget", event.currentTarget);
-        // console.log("target", event.target);
-        console.log(currentProduct);
-}
+// function handleClick(event) {
+//         if (event.target === event.currentTarget) {
+//                 return;
+//         }
+
+//         const currentProduct = event.target.closest(".js-product-item"); //цей метод повертає найближчий батьківський елемент з вказаним класом
+//         // console.log("currentTarget", event.currentTarget);
+//         // console.log("target", event.target);
+//         // console.log(currentProduct);
+
+//         const id = currentProduct.dataset.id;
+//         // console.log(id);
+
+//         // console.log(typeof id);
+
+//         const product = products.find(({ id: productId }) => productId === Number(id));
+
+//         // console.log(product);
+
+
+//         const instance = basicLightbox.create(`
+//         <div class="modal">
+//             <img src="${product.img}" alt="${product.name}"/>
+//             <h2>${product.name}</h2>
+//             <h3>${product.price}</h3>
+//             <p>${product.description}</p>
+//         </div>
+//         `)
+        
+//         instance.show()
+// }
