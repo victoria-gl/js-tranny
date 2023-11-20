fbc68cfbf289abb8d72e007b648040cc1bfbbb18
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-20 01:59:29 +0100

    tranny

--- tranny.js
+++ tranny.js
@@ -2026,21 +2026,131 @@
 
 //StopPropagation 
 
-const red = document.querySelector(".box-red")
-const green = document.querySelector(".box-green")
-const black = document.querySelector(".box-black")
+// const red = document.querySelector(".box-red")
+// const green = document.querySelector(".box-green")
+// const black = document.querySelector(".box-black")
 
-red.addEventListener("click", handleClick)
+// red.addEventListener("click", handleClick)
 // green.addEventListener("click", handleClick)
 // black.addEventListener("click", handleClick)
 
+// function handleClick(event) {
+//         const isConfirm = confirm(`click on element ${event.currentTarget.id} call stopPropagation`);
+
+//         if (isConfirm) {
+//                 event.stopPropagation()
+//         }
+// }
+
+
+
+// StopImmediatePropagation
+
+
+// const red = document.querySelector(".box-red")
+
+// red.addEventListener("click", firstClick)
+// red.addEventListener("click", secondClick)
+// red.addEventListener("click", thirdClick)
+
+// function firstClick(event) {
+//         console.log("firstClick");
+
+//         const isConfirm = confirm(`call stopImmediatePropagation`)
+
+//         if (isConfirm) {
+//                 event.stopImmediatePropagation()
+//         }
+// }
+
+// function secondClick(event) {
+//         console.log("secondClick");
+
+//         const isConfirm = confirm(`call stopImmediatePropagation`)
+
+//         if (isConfirm) {
+//                 event.stopImmediatePropagation()
+//         }
+// }
+
+// function thirdClick(event) {
+//         console.log("thirdClick");
+
+//         const isConfirm = confirm(`call stopImmediatePropagation`)
+
+//         if (isConfirm) {
+//                 event.stopImmediatePropagation()
+//         }
+// }
+
+
+// **************** Практика **************** \\
+// Створи картки з товарами на основі масиву products, 
+// приклад картки https://prnt.sc/KmgDlzqOIA3M
+// Реалізуй делегування подій на колекції карток
+// Після кліку на картку повинно з'являтись модальне вікно 
+// з детальною 
+// інформацією про продукт, приклад модального вікна https://prnt.sc/vWNoCeZcw7ii
+// Для реалізації модального вікна використай 
+// бібліотеку basicLightbox 
+// (https://github.com/electerious/basicLightbox)
+
+
+const products = [
+    {
+      id: 1,
+      img: "https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG",
+      name: "Монітор",
+      price: 3000,
+      description: "23-дюймовий монітор з Full HD роздільною здатністю.",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzWqRMI3HQiDfICHAmbArmaP4uOOIjfz0sDITv0dfkpb0mbbgX",
+      name: "Ноутбук",
+      price: 20000,
+      description: "Легкий та потужний ноутбук з 15-дюймовим дисплеєм та SSD.",
+    },
+    {
+      id: 3,
+      img: "https://cdn.27.ua/799/66/39/6841913_1.jpeg",
+      name: "Смартфон",
+      price: 8000,
+      description: "Оснащений потрійною камерою та багатоядерним процесором.",
+    },
+    {
+      id: 4,
+      img: "https://cdn.27.ua/799/b6/16/4371990_1.jpeg",
+      name: "Планшет",
+      price: 12000,
+      description:
+        "10-дюймовий планшет з високою продуктивністю та Retina дисплеєм.",
+    },
+];
+
+const container = document.querySelector(".js-products");
+
+container.insertAdjacentHTML("beforeend", createMarkup(products))
+container.addEventListener("click", handleClick)
+
+function createMarkup(arr) {
+        return arr.map(({ id, img, name, price, description }) => `
+                <li data-id="${id}" class="item js-product-item">
+                     <img src="${img}" alt="${name}" width="300px"/>
+                     <h2>${name}</h2>
+                     <p>Price: ${price}</p>
+                </li>
+        `).join("")
+};
+console.log(createMarkup(products));
+
 function handleClick(event) {
-        
+        if (event.target === event.currentTarget) {
+                return;
+        }
+
+        const currentProduct = event.target.closest(".js-product-item") //цей метод повертає найближчий батьківський елемент з вказаним класом
+        // console.log("currentTarget", event.currentTarget);
+        // console.log("target", event.target);
+        console.log(currentProduct);
 }
-
-
-
-
-
-
-
