e261387fcea1d3ac7205524b0cb9fb365db84eb4
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-20 21:49:06 +0100

    tranny

--- tranny.js
+++ tranny.js
@@ -2228,90 +2228,90 @@
 // Для виводу модального вікна застосуй бібліотеку basiclightbox
 // Після визначення переможця обов'язково підготуй ігрове поле для наступної гри
 
-const combination = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [1, 5, 9],
-    [3, 5, 7],
-    [3, 6, 9]
-]
+// const combination = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [1, 5, 9],
+//     [3, 5, 7],
+//     [3, 6, 9]
+// ]
 
-const content = document.querySelector(".content")
-const historyX = [];
-const historyO = [];
+// const content = document.querySelector(".content")
+// const historyX = [];
+// const historyO = [];
 
-let player = "X";
+// let player = "X";
 
-content.addEventListener("click", handleClick)
+// content.addEventListener("click", handleClick)
 
-function createMarkup() {
-        let markup = ""
-        for (let i = 1; i <= 9; i++){
-                markup += `<div class="item" data-id="${i}"></div>`
-        }
+// function createMarkup() {
+//         let markup = ""
+//         for (let i = 1; i <= 9; i++){
+//                 markup += `<div class="item" data-id="${i}"></div>`
+//         }
 
 
-        content.innerHTML = markup;
-}
+//         content.innerHTML = markup;
+// }
 
-createMarkup();
+// createMarkup();
 
-function handleClick(event) {
-        if (event.target === event.currentTarget || event.target.textContent) {
-                return;
-        }
+// function handleClick(event) {
+//         if (event.target === event.currentTarget || event.target.textContent) {
+//                 return;
+//         }
 
-        const id = +event.target.dataset.id // const id = Number(event.target.dataset.id)
+//         const id = +event.target.dataset.id // const id = Number(event.target.dataset.id)
         // console.log(id);
 
-        let isWinner = false;
+//         let isWinner = false;
 
-        if (player === "X") {
-                historyX.push(id);
-                isWinner = historyX.length >= 3 ? checkWinner(historyX) : false;
-        } else {
-                historyO.push(id);
-                isWinner = historyO.length >= 3 ? checkWinner(historyO) : false;
-        }
+//         if (player === "X") {
+//                 historyX.push(id);
+//                 isWinner = historyX.length >= 3 ? checkWinner(historyX) : false;
+//         } else {
+//                 historyO.push(id);
+//                 isWinner = historyO.length >= 3 ? checkWinner(historyO) : false;
+//         }
 
-        if (isWinner) {
-                const instance = basicLightbox.create(`
-                <div class="box">
-                          <h1>Player - ${player} is winner</h1>
-                </div>
-                `)
+//         if (isWinner) {
+//                 const instance = basicLightbox.create(`
+//                 <div class="box">
+//                           <h1>Player - ${player} is winner</h1>
+//                 </div>
+//                 `)
 
-                instance.show()
-                resetGame();
-                return;
-        }
+//                 instance.show()
+//                 resetGame();
+//                 return;
+//         }
 
 
-        event.target.textContent = player;
-        player = player === "X" ? "O" : "X";
+//         event.target.textContent = player;
+//         player = player === "X" ? "O" : "X";
 
-        const lose = [...content.children].every(item => item.textContent);
-        if (lose) {
-                resetGame();
-        }
-}
+//         const lose = [...content.children].every(item => item.textContent);
+//         if (lose) {
+//                 resetGame();
+//         }
+// }
         
 
-function checkWinner(arr) {
-        return combination.some(item => item.every(id => arr.includes(id)))
+// function checkWinner(arr) {
+//         return combination.some(item => item.every(id => arr.includes(id)))
         
-}
+// }
 
 // console.log([1, 2, 3].every(id => id > 2));
 
 
-function resetGame() {
-        createMarkup();
-        player = "X";
-        historyX.splice(0);
-        historyO.splice(0);
-}
+// function resetGame() {
+//         createMarkup();
+//         player = "X";
+//         historyX.splice(0);
+//         historyO.splice(0);
+// }
 
